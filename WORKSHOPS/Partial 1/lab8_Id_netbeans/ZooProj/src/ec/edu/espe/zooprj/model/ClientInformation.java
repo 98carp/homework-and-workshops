954cdf8a92191a527cc6bdb44631cfc51ad0b313
sebas1197 <sebas.landa1197@gmail.com>
2018-10-30 22:48:43 -0500

    Class and packages

---
+++ WORKSHOPS/Partial 1/lab8_Id_netbeans/ZooProj/src/ec/edu/espe/zooprj/model/ClientInformation.java
@@ -0,0 +1,93 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.zooprj.model;
+
+/**
+ *
+ * @author G403
+ */
+public class ClientInformation {
+    
+    private String name;
+    private String surname;
+    private String adress;
+    private int id;
+    private int cellphone;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the surname
+     */
+    public String getSurname() {
+        return surname;
+    }
+
+    /**
+     * @param surname the surname to set
+     */
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    /**
+     * @return the adress
+     */
+    public String getAdress() {
+        return adress;
+    }
+
+    /**
+     * @param adress the adress to set
+     */
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the cellphone
+     */
+    public int getCellphone() {
+        return cellphone;
+    }
+
+    /**
+     * @param cellphone the cellphone to set
+     */
+    public void setCellphone(int cellphone) {
+        this.cellphone = cellphone;
+    }
+    
+    
+}
---
+++ WORKSHOPS/Partial 1/lab8_Id_netbeans/ZooProj/src/zooproyect/ZooProject.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zooproyect;
+
+/**
+ *
+ * @author Sebastian Landazuri
+ */
+
+import ec.edu.espe.zooprj.model.ClientInformation;
+
+public class ZooProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private static String name = "Sebastian";
+    private static String surname = "landazuri";
+    private static String adress = "Quito";
+    private static int id = 1234567;
+    private static int cellphone = 234567;
+    
+    public static void main(String[] args) {
+        System.out.println("Hi mi name is "+name);
+        System.out.println("Surname: "+surname);
+        System.out.println("adress : "+adress);
+        System.out.println("id: "+id);
+        System.out.println("cellphone: "+cellphone);
+        
+        
+    }
+   
+    
+    
+    
+}
