d88b2cb2cbe7b5a9de696aae9d8b19bf4d339bca
sebas1197 <sebas.landa1197@gmail.com>
2018-11-23 20:48:47 -0500

    Lab16

--- WORKSHOPS/Partial 2/Lab16_Exceptions/src/ec/edu/espe/lab15/model/Animal.java
+++ WORKSHOPS/Partial 2/Lab16_Exceptions/src/ec/edu/espe/lab15/model/Animal.java
@@ -8,9 +8,21 @@ package ec.edu.espe.lab15.model;
 public class Animal {
  
     public String eyes;
+    public static int barnNumber=7;
+
+    public int getBarnNumber() {
+        return barnNumber;
+    }
+    
+    
     
     public void feed(){
         System.out.println("The animal is eating");
     }
+    
+    public  void asignBarnNumber(int bN){
+        barnNumber=bN;
+    }
+    
     
 }
--- WORKSHOPS/Partial 2/Lab16_Exceptions/src/lab16_exceptions/Lab16_Exceptions.java
+++ WORKSHOPS/Partial 2/Lab16_Exceptions/src/lab16_exceptions/Lab16_Exceptions.java
@@ -3,6 +3,7 @@ package lab16_exceptions;
 
 import ec.edu.espe.lab15.model.Animal;
 import ec.edu.espe.lab15.operations.Operation;
+import java.io.IOException;
 
 /**
  *
@@ -29,9 +30,56 @@ public class Lab16_Exceptions {
         result=operation.add(1.2f,2.2f);
         System.out.println("The result of float is: "+result);
         
+        System.out.println("");
         Animal animal = null;
+        animal = new Animal();
         
-        System.out.println("--> "+animal.eyes);
+        if(animal != null){
+            System.out.println("The animal Exist");
+        }else{
+            System.out.println("The animal does not exist");
+        }
+        
+        System.out.println("");
+        try{
+            System.out.println("The first animal runs");
+            System.out.println("--> "+animal.eyes);
+            System.out.println("The program is still running");
+        }catch(NullPointerException ex){
+           
+            animal = new Animal();
+            if(animal.eyes!=null){
+                System.out.println(""+animal.eyes);
+            }else{
+                animal.eyes="brown";           
+                System.out.println("The hourse has "+animal.eyes+" eyes");
+            }
+        
+            
+            
+            
+            /*System.out.println("An error ocurred "+ex);
+            System.out.println("The exception is running "+ex.getMessage());
+            System.out.println("StrackTrace  "+ex.getStackTrace());
+            System.out.println("StrackTrace error  ");
+            ex.printStackTrace();
+            if(ex.toString().equals("java.lang.NullPointerException")){
+                System.out.println("No animal created");
+            }*/
+        }finally{
+           // System.out.println("After the exception");
+            System.out.println("The hourse has brown eyes");
+        } 
+          Animal animal2 = new Animal();
+          
+          
+        System.out.println("This is the barn number "+animal.getBarnNumber());
+        
+         animal.asignBarnNumber(18);
+        
+        System.out.println("This is the barn number for animal 2 "+animal2.getBarnNumber());
+        
+       
         
         
         
